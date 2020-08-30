de964162f028127b07b5e3608ffe4db7885797b1
rakhmankulov1 <rakhmankulov-damir@mail.ru>
2020-08-30 20:00:44 +0300

    1.6. Взаимодействие объектов [#325791]

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void rolling() {
+    }
+
+    public void singing() {
+        System.out.println("Я от бабушки ушёл и от тебя уйду!");
+    }
+}
