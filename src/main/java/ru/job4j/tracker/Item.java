573d840bb498c536022c81208063701040b2bdba
rakhmankulov1 <rakhmankulov-damir@mail.ru>
2020-09-10 21:15:21 +0300

    7. Date. Отображение даты. [#325797]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,6 +1,9 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
+    private LocalDateTime created = LocalDateTime.now();
     private int id;
     private String name;
 
@@ -18,5 +21,9 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item firstTime = new Item();
+        LocalDateTime timeForFormat = firstTime.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String localDateTimeFormatted = timeForFormat.format(formatter);
+        System.out.println(localDateTimeFormatted);
+    }
+}
