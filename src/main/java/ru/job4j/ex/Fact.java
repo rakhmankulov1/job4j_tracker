064a4c85d99395ef03597e904e06346a45893c20
rakhmankulov1 <rakhmankulov-damir@mail.ru>
2020-12-22 12:51:31 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#325772]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,19 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+
+    }
+
+    public int calc(int n) {
+        int rsl = 1;
+        if (n < 0) {
+            throw new IllegalArgumentException("Parametr of the calc"
+                   + " method should be more than '0'");
+        }
+            for (int index = 1; index <= n; index++) {
+                rsl *= index;
+            }
+            return rsl;
+        }
+}
