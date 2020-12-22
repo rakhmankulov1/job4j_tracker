32b9da075015c0e46b2ae037a2acce119b842a11
rakhmankulov1 <rakhmankulov-damir@mail.ru>
2020-12-22 13:05:19 +0300

    0.3. Тестирование исключений с junit [#325773]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -1,19 +1,15 @@
 package ru.job4j.ex;
 
 public class Fact {
-    public static void main(String[] args) {
-
-    }
-
-    public int calc(int n) {
+    public static int calc(int n) {
         int rsl = 1;
         if (n < 0) {
             throw new IllegalArgumentException("Parametr of the calc"
-                   + " method should be more than '0'");
+                    + " method should be more than '0'");
         }
-            for (int index = 1; index <= n; index++) {
-                rsl *= index;
-            }
-            return rsl;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
         }
+        return rsl;
+    }
 }
