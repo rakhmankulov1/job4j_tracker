1449eaa2fe19afe80f67d46c478eac8b4a800360
rakhmankulov1 <rakhmankulov-damir@mail.ru>
2020-12-22 14:34:57 +0300

    0.4. Пользовательские исключения. [#325774]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,28 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        int index = 0;
+        /* for-each */
+        for (String allElements : value) {
+            if (allElements == key) {
+                rsl = index;
+            }
+            index++;
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("not such element found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] array ={"aa","ss","dd"};
+        try {
+            indexOf(array, "ff");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
