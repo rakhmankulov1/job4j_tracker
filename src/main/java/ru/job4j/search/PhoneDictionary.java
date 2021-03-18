120a556e0bb06910aad712002ec5a89fd0474b95
rakhmankulov1 <rakhmankulov-damir@mail.ru>
2021-03-18 15:46:20 +0300

    1. Телефонный справочник на базе ArrayList [#325731]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,29 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     *
+     * @param key Ключ поиска.
+     * @return Список подощедщих пользователей.
+     */
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person names:persons) {
+           if ((names.getName().contains(key)) || (names.getSurname().contains(key))
+                   || (names.getPhone().contains(key)) || (names.getAddress().contains(key))) {
+               result.add(names);
+           }
+        }
+
+        return result;
+    }
+}
