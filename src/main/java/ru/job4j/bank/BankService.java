fa6624f790328c00b84cba99a4628b198869fb7c
rakhmankulov1 <rakhmankulov-damir@mail.ru>
2021-04-05 20:13:07 +0300

    3. Что такое JavaDoc [#325718]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -5,13 +5,43 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Класс описывает главный сервис для банковской системы.
+ * В системе можно производить следующие действия:
+ *
+ * 1. Регистрировать пользователя.
+ * 2. Удалять пользователя из системы.
+ * 3. Добавлять пользователю банковский счет. У пользователя системы
+ * могут быть несколько счетов.
+ * 4. Переводить деньги с одного банковского счета на другой счет.
+ * @author Damir Rakhmankulov
+ * @version 1.0
+ */
 public class BankService {
+    /**
+     * Хранение пользователей осуществляется в коллекции типа HashMap.
+     */
     private Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Метод принимает на вход пользователя проверяет существует ли
+     * такой же, если такого же пользователя нет,
+     * то добавляет его в коллекцию.
+     * @param user пользователь который добавляется в коллекцию.
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<>());
     }
 
+    /**
+     * Метод принимает на вход номер пасспорта и аккаунт. Осуществляет
+     * поиск пользователя по номеру пасспорта
+     * в коллекции и если находит, то сравнивает аккаунт принятый на
+     * вход метода с аккаунтами пользователя, при
+     * отсутствии совпадений добавляет пользователю аккаунт.
+     * @param passport номер пасспорта пользователя
+     * @param account новый аккаунт для пользователя
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -22,6 +52,13 @@ public class BankService {
         }
     }
 
+    /**
+     * Метод осуществляет поиск пользователя в коллекции по номеру
+     * пасспорта
+     * @param passport номер пасспорта пользователя
+     * @return возвращает найденного в коллекции пользователя или
+     * Null если пользователь не найден.
+     */
     public User findByPassport(String passport) {
         User rsl = null;
         for (User user : users.keySet()) {
@@ -33,6 +70,15 @@ public class BankService {
         return rsl;
     }
 
+    /**
+     * Метод осуществляет поиск аккаунта в коллекции по номеру
+     * пасспорта пользователя и реквизиту аккаунта
+     * @param passport номер пасспорта пользователя
+     * @param requisite реквизит из аккаунта пользователя
+     * @return возвращает найденный в коллекции аккаунт или Null
+     * если не будет найден пользователь с номером пасспорта
+     * и реквизитом аккаунта пришедшими на вход метода.
+     */
     public Account findByRequisite(String passport, String requisite) {
         Account foundAccount = null;
         User user = findByPassport(passport);
@@ -48,6 +94,25 @@ public class BankService {
         return foundAccount;
     }
 
+    /**
+     * Метод принимает на вход сумму перевода, номера пасспортов
+     * и реквизиты аккаунтов пользователей,
+     * и осуществляет перевод между ними в случае прохождения
+     * проверок.
+     * Проверки происходят на наличие в коллекции аккаунтов
+     * отправителя и получателя, а также на соответствие
+     * размера баланса отправителя возможности осуществить
+     * перевод получателю.
+     * @param srcPassport номер пасспорта пользователя отправителя
+     * перевода
+     * @param srcRequisite реквизиты аккаунта отправителя
+     * @param destPassport номер пасспорта пользователя получателя
+     * перевода
+     * @param destRequisite реквизиты аккаунта получателя
+     * @param amount сумма перевода
+     * @return возвращает true если перевод осуществлен и false
+     * если проверки не пройдены и перевод не осуществлялся.
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
