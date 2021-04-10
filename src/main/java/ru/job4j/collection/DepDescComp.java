1e4b65ab9e26194602c834349fb3f396a7a76d3e
rakhmankulov1 <rakhmankulov-damir@mail.ru>
2021-04-10 21:41:04 +0300

    Отсортировать департаменты [#325721] исправление 2

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -7,19 +7,7 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String o1, String o2) {
         String[] array1 = o1.split("/");
         String[] array2 = o2.split("/");
-        int result = 0;
-        result = array2[0].compareTo(array1[0]);
-        if (result == 0) {
-            int size = Math.min(array1.length, array2.length);
-            if (array1.length == array2.length) {
-                for (int index = 0; index < size; index++) {
-                    result = array1[index].compareTo(array2[index]);
-                }
-                return result;
-            } else {
-                return Integer.compare(array1.length, array2.length);
-            }
-        }
-        return result;
+        int result = array2[0].compareTo(array1[0]);
+        return result == 0 ? o1.compareTo(o2) : result;
     }
 }
