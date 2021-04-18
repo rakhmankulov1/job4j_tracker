5bfc59003d34edcef3a128e6578e534414b9c4af
rakhmankulov1 <rakhmankulov-damir@mail.ru>
2021-04-18 20:44:30 +0300

    2. Функциональный интерфейс [#325570] исправление

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -29,16 +29,20 @@ public class DepartmentsTest {
 
     @Test
     public void whenMissedFromTask() {
-        List<String> input = Arrays.asList("K1/SK1", "K1/SK2", "K1/SK1/SSK1", "K1/SK1/SSK2", "K2", "K2/SK1/SSK1", "K2/SK1/SSK2");
-        List<String> expect = Arrays.asList("K1", "K1/SK1", "K1/SK2", "K1/SK1/SSK1", "K1/SK1/SSK2", "K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2");
+        List<String> input = Arrays.asList("K1/SK1", "K1/SK2", "K1/SK1/SSK1", "K1/SK1/SSK2",
+                "K2", "K2/SK1/SSK1", "K2/SK1/SSK2");
+        List<String> expect = Arrays.asList("K1", "K1/SK1", "K1/SK2", "K1/SK1/SSK1",
+                "K1/SK1/SSK2", "K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenMissedFromTaskAndReverse() {
-        List<String> input = Arrays.asList("K1/SK1", "K1/SK2", "K1/SK1/SSK1", "K1/SK1/SSK2", "K2", "K2/SK1/SSK1", "K2/SK1/SSK2");
-        List<String> expect = Arrays.asList("K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2", "K1", "K1/SK1", "K1/SK2", "K1/SK1/SSK1", "K1/SK1/SSK2");
+        List<String> input = Arrays.asList("K1/SK1", "K1/SK2", "K1/SK1/SSK1", "K1/SK1/SSK2",
+                "K2", "K2/SK1/SSK1", "K2/SK1/SSK2");
+        List<String> expect = Arrays.asList("K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2",
+                "K1", "K1/SK1", "K1/SK2", "K1/SK1/SSK1", "K1/SK1/SSK2");
         List<String> result = Departments.fillGaps(input);
         result.sort(new DepDescComp());
         assertThat(result, is(expect));
