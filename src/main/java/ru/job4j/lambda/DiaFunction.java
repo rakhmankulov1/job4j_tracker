beddeb9ea7f31062b3820bebabf5b0288778d6de
rakhmankulov1 <rakhmankulov-damir@mail.ru>
2021-04-24 13:22:55 +0300

    5. Подсчет функции в диапазоне. [#325564] исправление

--- src/main/java/ru/job4j/lambda/DiaFunction.java
+++ src/main/java/ru/job4j/lambda/DiaFunction.java
@@ -7,10 +7,8 @@ import java.util.function.Function;
 public class DiaFunction {
     static List<Double> diapason(int start, int end, Function<Double, Double> func) {
         List<Double> rsl = new ArrayList<>();
-        double x = 0;
         for (int i = start; i < end; i++) {
-            x = i;
-            rsl.add(func.apply(x));
+            func.andThen(rsl::add).apply((double) i);
         }
         return rsl;
     }
