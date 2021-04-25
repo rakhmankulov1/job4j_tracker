9e340fcf47a13bddc177297d72dbb18a9be3100e
rakhmankulov1 <rakhmankulov-damir@mail.ru>
2021-04-25 10:48:13 +0300

    0. Stream API [#325578]

---
+++ src/main/java/ru/job4j/stream/IntegerStream.java
@@ -0,0 +1,18 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class IntegerStream {
+    public static void main(String[] args) {
+        List<Integer> intStream = new ArrayList<>();
+        intStream.add(0, -1);
+        intStream.add(1, 0);
+        intStream.add(2, 1);
+        List<Integer> positive = intStream.stream()
+                .filter(ints -> ints > 0)
+                .collect(Collectors.toList());
+        positive.forEach(System.out::println);
+    }
+}
