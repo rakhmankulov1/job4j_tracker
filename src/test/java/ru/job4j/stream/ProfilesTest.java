e4fa3ce8853935b6d65791601e8a17bd17ce8fc3
rakhmankulov1 <rakhmankulov-damir@mail.ru>
2021-04-28 14:30:35 +0300

    2. Список адресов [#325580]

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ProfilesTest {
+
+    @Test
+    public void collectAddress() {
+        List<Profile> profiles = List.of(
+                new Profile(new Address("New York", "Congress", 1, 13)),
+                new Profile(new Address("Jersey", "Main", 5, 77)),
+                new Profile(new Address("Denver", "Coronado", 3, 1))
+        );
+        Profiles profiles1 = new Profiles();
+
+        List<Address> rsl = profiles1.collect(profiles);
+        List<Address> expected = new ArrayList<>();
+        expected.add(new Address("New York", "Congress", 1, 13));
+        expected.add(new Address("Jersey", "Main", 5, 77));
+        expected.add(new Address("Denver", "Coronado", 3, 1));
+        assertThat(rsl, is(expected));
+    }
+}
