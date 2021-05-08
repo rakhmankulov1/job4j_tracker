48f581126c3abeee35ceb0ce313581dd5dba5e1f
rakhmankulov1 <rakhmankulov-damir@mail.ru>
2021-05-08 12:41:02 +0300

    8. Перенос методов в Stream API [#325586] исправление 3

--- src/main/java/ru/job4j/stream/Card.java
+++ src/main/java/ru/job4j/stream/Card.java
@@ -3,12 +3,12 @@ package ru.job4j.stream;
 import java.util.stream.Stream;
 
 public class Card {
-    private static Suit suit;
-    private static Value value;
+    private Suit suit;
+    private Value value;
 
     public Card(Suit suit, Value value) {
-        Card.suit = suit;
-        Card.value = value;
+        this.suit = suit;
+        this.value = value;
     }
 
     @Override
